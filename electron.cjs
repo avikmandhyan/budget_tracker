a4dc6432a2542e47930098cbcf9d7ea5ff8848e9
avikmandhyan <avikmandhyan9@gmail.com>
2026-05-28 02:17:17 +0530

    Add files via upload

---
+++ electron.cjs
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 420,
+    height: 820,
+    titleBarStyle: 'hiddenInset',
+    webPreferences: { nodeIntegration: false, contextIsolation: true }
+  })
+  win.loadFile(path.join(__dirname, 'dist/index.html'))
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() })
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow() })
+
+
+
+
